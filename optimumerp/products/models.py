80398abecf25185f50cd268ec280cc418fd371ed
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-04 11:34:17 -0300

    creating the Product model

--- optimumerp/products/models.py
+++ optimumerp/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
+    sale_price = models.FloatField()
+    is_perishable = models.BooleanField()
+    expiration_date = models.DateField(null=True, blank=True)
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
