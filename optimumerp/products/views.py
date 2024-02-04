7a3906ada19c082d70366ecbbb41181bd86087ce
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-04 12:38:31 -0300

    creating the views for produts app

--- optimumerp/products/views.py
+++ optimumerp/products/views.py
@@ -1,3 +1,101 @@
 from django.shortcuts import render
+from .models import Product
+from django.db.models import Q 
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    products = Product.objects.order_by("-id")
+
+    # Aplicando a paginação
+    paginator = Paginator(products, 100)
+    # /fornecedores?page=1 -> Obtendo a página da URL
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "products": page_obj,
+        }
+    
+    return render(request, "products/index.html", context)
+
+def search(request):
+    # Obtendo o valor da requisição (Formulário)
+    search_value = request.GET.get("q").strip()
+
+    # Verificando se algo foi digitado
+    if not search_value:
+        return redirect("products:index")
+    
+    # Filtrando os produtos
+    #  O Q é usado para combinar filtros (& ou |)
+    products = Product.objects\
+        .filter(Q(name__icontains=search_value))\
+        .order_by("-id")
+
+    paginator = Paginator(products, 100)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = { "products": page_obj}
+
+    return render(request, "products/index.html", context)
+
+def create(request):
+    form_action = reverse("products:create")
+    # POST
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, "Produto cadastrado com sucesso!")
+            
+            return redirect("products:index")
+                
+        messages.error(request, "Falha ao cadastrar o produto. Verifique o preenchimento dos campos.")
+        
+        context = { "form": form, "form_action": form_action }
+        
+        return render(request, "products/create.html", context)
+
+    # GET
+    form = ProductForm()
+
+    context = {
+        "form": form, 
+        "form_action": form_action,
+        }
+
+    return render(request, "products/create.html", context)
+
+def update(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    form_action = reverse("products:update", args=(slug,))
+
+    # POST
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            messages.success(request, "Produto atualizado com sucesso!")
+            return redirect("products:index")
+        
+        context = {
+            "form_action": form_action,
+            "form": form
+        }
+
+        return render(request, "products/create.html", context)
+    
+    # GET
+    form =  ProductForm(instance=product)
+
+    context = {
+        "form_action": form_action,
+        "form": form,
+    }
+
+    return render(request, "products/create.html", context)
