549df29f0548c52e8cb482f8ff825e22f98e3685
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-04 19:27:53 -0300

    Creating a measurement_choices dict and the field in Product model

--- optimumerp/products/models.py
+++ optimumerp/products/models.py
@@ -3,12 +3,19 @@ from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
+    MEASUREMENT_CHOICES = {
+        "KG": "Quilograma",
+        "Metro": "Metro",
+        "Litro": "Litro",
+        "Unidade": "Unidade"
+    }
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     slug = models.SlugField(unique=True, blank=True)
     sale_price = models.FloatField()
     is_perishable = models.BooleanField()
     expiration_date = models.DateField(null=True, blank=True)
+    unit_of_measurement = models.CharField(max_length=7, choices=MEASUREMENT_CHOICES)
     enabled = models.BooleanField(default=True)
 
     def __str__(self):
