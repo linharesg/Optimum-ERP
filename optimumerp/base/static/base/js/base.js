f1960d7d94bfae7a9cdad1bde00ad263c59abdd6
Abner Eger <abner.juda.eger.santana@gmail.com>
2024-02-08 22:18:02 -0300

    feat: :lipstick: updating the navbar and its animation

--- optimumerp/base/static/base/js/base.js
+++ optimumerp/base/static/base/js/base.js
@@ -1,4 +1,5 @@
 jQuery(function() {
+    // Link ativo
     const currentLocation = location.pathname;
     const $menuItems = $(".nav-link");
 
@@ -8,10 +9,24 @@ jQuery(function() {
         if ($menu.attr("href") === currentLocation) {
             $menu.attr("class", "nav-link active");
         } else {
-            $menu.attr("class", "nav-link link-dark");
+            $menu.attr("class", "nav-link link-light");
         }
     });
 
     // Habilitar os toats de notificação
     $(".toast").toast("show");
+
+    // Navbar animation
+    const $navbar = $("#navbar");
+    const $menuIcons = $(".menu-icons");
+    const $linkName = $(".link-name");
+
+    $navbar.hover(function() {
+        $(this).stop().animate({ width: '200px' }, 300) // Anima a largura para 150px ao passar o mouse
+        $linkName.css("display", "flex");
+        console.log($linkName.text())
+    }, function() {
+        $(this).stop().animate({ width: '80px' }, 300) // Anima a largura para 80px ao retirar o mouse
+        $linkName.css("display", "none");
+    });
 });
