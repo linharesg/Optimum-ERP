0416a1f88c363221ce9f91f6ee27bca244ab3a39
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-11 13:39:56 -0300

    updating transactions type, index and create.html, views

--- optimumerp/transactions/views.py
+++ optimumerp/transactions/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import Transaction
+from products.models import ProductInventory
 from .forms import TransactionsForm
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.db.models import Q
 
 def index(request):
     transactions = Transaction.objects.order_by("-id")
+    product_inventory = ProductInventory.objects.all()
 
-    paginator = Paginator(transactions, 100)
+    paginator = Paginator(transactions, 2)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     
     context = {
-        "transactions": page_obj
+        "transactions": page_obj,
+        "product_inventory": product_inventory
     }
     
     return render(request, "transactions/index.html", context)
@@ -24,7 +28,7 @@ def create(request):
         
         if form.is_valid():
             form.save()
-            messages.success(request, "Produto cadastrado com sucesso!")
+            messages.success(request, "Transação cadastrada com sucesso!")
             
             return redirect("transactions:index")
                 
@@ -44,5 +48,20 @@ def create(request):
     return render(request, "transactions/create.html", context)
 
 def search(request):
-    ...
+    search_value = request.GET.get("q").strip()
+
+    if not search_value:
+        return redirect("transactions:index")
+    
+    transactions = Transaction.objects.filter(Q(product__icontains=search_value) | Q(quantity__icontains=search_value)).order_by("-id")
+
+    paginator = Paginator(transactions, 2)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "transactions": page_obj
+    }
+    
+    return render(request, "transactions/index.html", context)
     
