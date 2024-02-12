a43a4d55a02fabef22d464a9d829115b3d4ccf2f
Abner Eger <abner.juda.eger.santana@gmail.com>
2024-02-12 11:50:56 -0300

    feat: :lipstick: add titlebar and account button

--- optimumerp/base/static/base/js/base.js
+++ optimumerp/base/static/base/js/base.js
@@ -18,15 +18,19 @@ jQuery(function() {
 
     // Navbar animation
     const $navbar = $("#navbar");
-    const $menuIcons = $(".menu-icons");
     const $linkName = $(".link-name");
-
+    const $titlebar = $(".titlebar");
+ 
     $navbar.hover(function() {
         $(this).stop().animate({ width: '200px' }, 300) // Anima a largura para 150px ao passar o mouse
-        $linkName.css("display", "flex");
-        console.log($linkName.text())
+        $titlebar.stop().animate({ width: 'calc(100vmax - 205px)'}, 300)
+        $linkName.fadeIn(300);
+        $linkName.css({
+            "display": "inline-block",
+        });
     }, function() {
         $(this).stop().animate({ width: '80px' }, 300) // Anima a largura para 80px ao retirar o mouse
-        $linkName.css("display", "none");
+        $titlebar.stop().animate({ width: 'calc(100vmax - 85px)'}, 300)
+        $linkName.fadeOut(300);
     });
 });
