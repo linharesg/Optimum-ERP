747fc4505e99bd327d5ee53593e438eac9f957c1
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-20 22:26:20 -0300

    creating create_invoice view

--- optimumerp/invoices/urls.py
+++ optimumerp/invoices/urls.py
@@ -5,4 +5,5 @@ app_name = "invoices"
 
 urlpatterns= [
     path("<int:id>/", views.open_invoice, name="open_invoice"),
+    path("create-invoice/<int:id>/", views.create_invoice, name="create_invoice")
 ]
--- optimumerp/invoices/views.py
+++ optimumerp/invoices/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Invoice
 from sales_order.models import SalesOrder, SalesOrderProduct
 from clients.models import Clients
@@ -8,15 +8,24 @@ def open_invoice(request, id):
     sale_order = get_object_or_404(SalesOrder, pk=id)
     sale_order_products = SalesOrderProduct.objects.filter(sale_order=sale_order)
     invoice = Invoice.objects.get(sale_order=sale_order)
-    access_key = f"{randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} \
-        {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)}"
-    barcode = f"{randint(100000000, 999999999)}"
     context = {
-        "sales_order": sale_order,
+        "sale_order": sale_order,
         "sale_order_products": sale_order_products,
-        "invoice": invoice,
-        "access_key": access_key,
-        "barcode": barcode
+        "invoice": invoice
         }
     
     return render(request, "invoices/invoice.html", context)
+
+def create_invoice(request, id):
+    sale_order = get_object_or_404(SalesOrder, pk=id)
+    try:
+        invoice = Invoice.objects.get(sale_order=sale_order)
+    except:
+        sale_order = get_object_or_404(SalesOrder, pk=id)
+        access_key = f"{randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)}\
+            {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)}"
+        barcode = f"{randint(100000000, 999999999)}"
+        Invoice.objects.create(sale_order=sale_order, access_key=access_key, barcode=barcode)
+    
+        return redirect("invoices:open_invoice", id=sale_order.id)
+    return redirect("invoices:open_invoice", id=sale_order.id)
