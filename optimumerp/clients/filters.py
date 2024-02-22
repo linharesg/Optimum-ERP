6be9ffec3fd819ababea2c1efbf197b3b89e226d
Gabriel Linhares <linhares.gs96@gmail.com>
2024-02-22 12:19:43 -0300

    fix: include lookup_expr='icontains' in client fields filter

--- optimumerp/clients/filters.py
+++ optimumerp/clients/filters.py
@@ -7,11 +7,11 @@ from django import forms
 from django.forms.widgets import TextInput, Select
 
 class ClientsFilter(django_filters.FilterSet):
-    company_name = django_filters.CharFilter(field_name='company_name', label="", widget=TextInput(attrs={'placeholder': 'Razão social'}))
+    company_name = django_filters.CharFilter(field_name='company_name', label="", widget=TextInput(attrs={'placeholder': 'Razão social'}), lookup_expr='icontains')
     state = django_filters.ChoiceFilter(field_name='state', label="Estado", choices=Clients.STATE_CHOICES)
-    city = django_filters.CharFilter(field_name='city',label="", widget=TextInput(attrs={'placeholder': 'Cidade'}))
+    city = django_filters.CharFilter(field_name='city',label="", widget=TextInput(attrs={'placeholder': 'Cidade'}), lookup_expr='icontains')
     cnpj = django_filters.CharFilter(field_name='cnpj',label="", widget=TextInput(attrs={'placeholder': 'CNPJ'}))
-    email = django_filters.CharFilter(field_name='email',label="", widget=TextInput(attrs={'placeholder': 'email@email.com'}))
+    email = django_filters.CharFilter(field_name='email',label="", widget=TextInput(attrs={'placeholder': 'email@email.com'}), lookup_expr='icontains')
     enabled = django_filters.ChoiceFilter(field_name='enabled', label="Status", choices={True:"Ativo", False: "Inativo"})
     #  widget do select
     class Meta:
