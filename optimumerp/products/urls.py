78fbe4b80676d2f6df72b71da18f4790c3c6bd6b
Tamires <tp.tamirespereira@gmail.com>
2024-02-25 15:11:41 -0300

    feat: :sparkles: changing url from categories to CategoryListView

--- optimumerp/products/urls.py
+++ optimumerp/products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import CategoryListView
 
 app_name = "products"
 urlpatterns = [
@@ -11,7 +12,7 @@ urlpatterns = [
     path("<int:id>/delete", views.delete, name="delete"),
     path("<int:id>/suppliers/", views.get_suppliers_from_product, name="suppliers"),
     path("<int:id>/delete_supplier", views.delete_supplier_from_product, name="delete_supplier"),
-    path("categorias/", views.get_categories, name="categories"),
+    path("categorias/", CategoryListView.as_view(), name="categories"),
     path("categories/search", views.search_categories, name="categories_search"),
     path("categorias/cadastro/", views.create_category, name="category_create"),
     path("categorias/<int:id>/", views.update_category, name="category_update"),
