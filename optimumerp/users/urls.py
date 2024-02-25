a3f3a8ce7ff1a93f35b6030dcafd7957dfa658ad
Tamires <tp.tamirespereira@gmail.com>
2024-02-25 15:50:47 -0300

    feat: :sparkles: adding employee group

--- optimumerp/users/urls.py
+++ optimumerp/users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
+from .views import UserLoginView, UserLogoutView, EmployeeCreateView
 
 app_name = 'users'
 urlpatterns = [
     path("", RedirectView.as_view(url="/login/", permanent=True)),
     path('usuarios/', views.index, name="index"),
-    path("usuarios/cadastro/", views.create, name="create"),
-    path('login/', views.login, name="login"), 
+    path("usuarios/cadastro/", EmployeeCreateView.as_view(), name="create"),
+    path('login/', UserLoginView.as_view(), name="login"), 
     path('usuarios/<str:username>/', views.update, name="update"),
     path('usuarios/<int:id>/delete', views.delete, name="delete"),
-    path('logout/', views.logout, name="logout"),
+    path('logout/', UserLogoutView.as_view(), name="logout"),
 ]
