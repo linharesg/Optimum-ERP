caf4e1508e5c89e3a10464cd1299ea3b6735c14a
Tamires <tp.tamirespereira@gmail.com>
2024-02-27 00:12:59 -0300

    feat: :sparkles: changing views and index to use dash_app

--- optimumerp/dashboard/dash_app.py
+++ optimumerp/dashboard/dash_app.py
@@ -5,10 +5,35 @@ from django_plotly_dash import DjangoDash
 from django.db.models import Count, Sum
 from suppliers.models import Suppliers
 from purchases.models import Purchases
-from sales_order.models import SalesOrder
+from inventory.models import Inventory
+from sales_order.models import SalesOrder, SalesOrderProduct
 
 # Crie um aplicativo Dash
 app = DjangoDash("Dashboard")
+
+# GRÁFICO DE FORNECEDORES POR ESTADO
+@app.dash_app.callback(
+    dash.dependencies.Output('suppliers-chart', 'figure'),
+    []
+)
+def update_suppliers_chart():
+    suppliers_by_state = Suppliers.objects.values('state').annotate(count=Count('id'))
+    states = [s['state'] for s in suppliers_by_state]
+    counts = [s['count'] for s in suppliers_by_state]
+
+    data = [
+        go.Bar(
+            x=states,
+            y=counts,
+        )
+    ]
+
+    layout = go.Layout(
+        xaxis=dict(title='Estado'),
+        yaxis=dict(title='Número de Fornecedores')
+    )
+
+    return {'data': data, 'layout': layout}
 
 # GRÁFICO DE COMPRAS POR FORNECEDOR
 @app.dash_app.callback(
@@ -38,7 +63,7 @@ def update_purchases_chart():
     return {'data': data, 'layout': layout}
 
 
-# GRÁFICO DE VENDAS POR CLIENTE
+# GRÁFICO DE TOP 10 CLIENTES QUE MAIS COMPRARAM
 @app.dash_app.callback(
     dash.dependencies.Output('sales-chart', 'figure'),
     []
@@ -66,42 +91,110 @@ def update_sales_chart():
 
     return {'data': data, 'layout': layout}
 
-
-# GRÁFICO DE FORNECEDORES POR ESTADO
+# TOP 10 PRODUTOS MAIS VENDIDOS
 @app.dash_app.callback(
-    dash.dependencies.Output('suppliers-chart', 'figure'),
+    dash.dependencies.Output('top-products-chart', 'figure'),
     []
 )
-def update_suppliers_chart():
-    suppliers_by_state = Suppliers.objects.values('state').annotate(count=Count('id'))
-    states = [s['state'] for s in suppliers_by_state]
-    counts = [s['count'] for s in suppliers_by_state]
+def create_top_products_chart():
+    # Consulta para obter os top 10 produtos mais vendidos
+    top_products = SalesOrderProduct.objects.values('product__name').annotate(total_sold=Sum('amount')).order_by('-total_sold')[:10]
+    print(top_products)
+    # Extraia os nomes dos produtos e as quantidades vendidas
+    product_names = [item['product__name'] for item in top_products]
+    total_sold = [item['total_sold'] for item in top_products]
 
+    # Crie o gráfico de barras
     data = [
         go.Bar(
-            x=states,
-            y=counts,
+            x=total_sold,
+            y=product_names,
+            orientation='v'
         )
     ]
-
     layout = go.Layout(
-        xaxis=dict(title='Estado'),
-        yaxis=dict(title='Número de Fornecedores')
+        title='Top 10 Produtos Mais Vendidos',
+        xaxis=dict(title='Quantidade Vendida'),
+        yaxis=dict(title='Produto')
     )
 
     return {'data': data, 'layout': layout}
 
 
+# GRÁFICO DE VENDAS POR USUÁRIO
+@app.dash_app.callback(
+    dash.dependencies.Output('sales-by-user-chart', 'figure'),
+    []
+)
+def update_sales_by_user_chart():
+    # Consulta para obter as vendas por usuário
+    sales_by_user = SalesOrder.objects.values('user__name').annotate(total_sales=Sum('total_value'))
+
+    # Extraia os nomes dos usuários e os valores totais de vendas
+    name = [item['user__name'] for item in sales_by_user]
+    total_sales = [item['total_sales'] for item in sales_by_user]
+
+    # Crie o gráfico de pizza
+    data = [
+        go.Pie(
+            labels=name,
+            values=total_sales,
+        )
+    ]
+    layout = go.Layout(
+        title='Vendas por Usuário'
+    )
+
+    return {'data': data, 'layout': layout}
+
+# GRÁFICO DE TOP 10 PRODUTOS COM MAIOR ESTOQUE
+@app.dash_app.callback(
+    dash.dependencies.Output('top-inventory-chart', 'figure'),
+    []
+)
+def update_top_inventory_chart():
+    # Consulta para obter os top 10 produtos com maior estoque
+    top_inventory = Inventory.objects.order_by('-quantity')[:10]
+
+    # Extraia os nomes dos produtos e as quantidades em estoque
+    product_names = [item.product.name for item in top_inventory]
+    quantities = [item.quantity for item in top_inventory]
+
+    # Crie o gráfico de barras
+    data = [
+        go.Bar(
+            x=quantities,
+            y=product_names,
+            orientation='h'
+        )
+    ]
+    layout = go.Layout(
+        title='Top 10 Produtos com Maior Estoque',
+        xaxis=dict(title='Quantidade em Estoque'),
+        yaxis=dict(title='Produto')
+    )
+
+    return {'data': data, 'layout': layout}
+
 # Crie o layout do Dash
 app.layout = html.Div([
     html.H1('Dashboard'),
 
+    # GRÁFICO DE TOP 10 CLIENTES QUE MAIS COMPRARAM
+    dcc.Graph(id='sales-chart'),
+        
+    # GRÁFICO DE TOP 10 PRODUTOS MAIS VENDIDOS
+    dcc.Graph(id='top-products-chart'),
+
+    # GRÁFICO DE VENDAS POR POR USUÁRIO
+    dcc.Graph(id='sales-by-user-chart'),
+
+    # GRÁFICO DE FORNECEDORES POR ESTADO
+    dcc.Graph(id='suppliers-chart'),
+    
     # GRÁFICO DE COMPRAS POR FORNECEDOR
     dcc.Graph(id='purchases-chart'),
 
-    # GRÁFICO DE VENDAS POR CLIENTE
-    dcc.Graph(id='sales-chart'),
-
-    # GRÁFICO DE FORNECEDORES POR ESTADO
-    dcc.Graph(id='suppliers-chart')
+    # GRÁFICO DE TOP 10 PRODUTOS COM MAIOR ESTOQUE
+    dcc.Graph(id='top-inventory-charts')
 ])
--- optimumerp/dashboard/views.py
+++ optimumerp/dashboard/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import TemplateView
 from plotly.offline import plot
-from plotly.graph_objects import Bar
+from plotly.graph_objects import Bar, Pie
 from django.db.models import Sum
 from suppliers.models import Suppliers
 from purchases.models import Purchases
-from sales_order.models import SalesOrder
+from inventory.models import Inventory
+from sales_order.models import SalesOrder, SalesOrderProduct
 
 class DashView(TemplateView):
     template_name = "dashboard/index.html"
@@ -30,8 +31,30 @@ class DashView(TemplateView):
         counts = [s['count'] for s in suppliers_by_state]
         plot_div_suppliers = plot([Bar(x=states, y=counts)], output_type="div", include_plotlyjs=False)
 
+        # Gráfico Top 10 produtos mais vendidos
+        top_products = SalesOrderProduct.objects.values('product__name').annotate(total_sold=Sum('amount')).order_by('-total_sold')[:10]
+        product_names = [item['product__name'] for item in top_products]
+        total_sold = [item['total_sold'] for item in top_products]
+        plot_div_salesproduct = plot([Bar(x=total_sold, y=product_names, orientation='v')], output_type="div", include_plotlyjs=False)
+
+        # Gráfico Vendas por Usuário/Vendedor
+        sales_by_user = SalesOrder.objects.values('user__name').annotate(total_sales=Sum('total_value'))
+        name = [item['user__name'] for item in sales_by_user]
+        total_sales = [item['total_sales'] for item in sales_by_user]
+        plot_div_salesusers = plot([Pie(labels=name, values=total_sales)], output_type="div", include_plotlyjs=False)
+
+        # Gráfico Top 10 Produtos com maior estoque
+        top_inventory = Inventory.objects.order_by('-quantity')[:10]
+        product_names = [item.product.name for item in top_inventory]
+        quantities = [item.quantity for item in top_inventory]
+        plot_div_inventory = plot([Bar(x=quantities, y=product_names, orientation='h')], output_type="div", include_plotlyjs=False)
+
+
         context["plot_div_purchases"] = plot_div_purchases
         context["plot_div_sales"] = plot_div_sales
         context["plot_div_suppliers"] = plot_div_suppliers
+        context["plot_div_salesproduct"] = plot_div_salesproduct
+        context["plot_div_salesusers"] = plot_div_salesusers
+        context["plot_div_inventory"] = plot_div_inventory
 
         return context
