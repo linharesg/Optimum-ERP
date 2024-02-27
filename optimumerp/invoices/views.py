6fee1045751834c4d4533ac5e23eebc018750129
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2024-02-26 21:56:34 -0300

    updating invoice create view and model

--- optimumerp/invoices/views.py
+++ optimumerp/invoices/views.py
@@ -33,14 +33,63 @@ def open_invoice(request, id):
 
 def create_invoice(request, id):
     sale_order = get_object_or_404(SalesOrder, pk=id)
+    companies = Company.objects.all()
+    client = sale_order.client
+    acess_key_list = []
+    for i in range(0, 10):
+        acess_key_list.append(f"{randint(1000,9999)}")
+    acess_key = (' '.join(acess_key_list))
     try:
         invoice = Invoice.objects.get(sale_order=sale_order)
     except:
         sale_order = get_object_or_404(SalesOrder, pk=id)
-        access_key = f"{randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)}\
-            {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)} {randint(1000, 9999)}"
+        access_key = acess_key
         barcode = f"{randint(100000000, 999999999)}"
-        Invoice.objects.create(sale_order=sale_order, access_key=access_key, barcode=barcode)
-    
+        for company in companies:
+            emitter_name = company.name
+            emitter_fantasy_name = company.fantasy_name
+            emitter_state_registration = company.state_registration
+            emitter_cnpj = company.cnpj
+            emitter_email = company.email
+            emitter_zipcode = company.zipcode
+            emitter_street = company.street
+            emitter_number = company.number
+            emitter_city = company.city
+            emitter_state = company.state
+            emitter_phone = company.phone
+            receiver_name = client.company_name
+            receiver_fantasy_name = client.fantasy_name
+            receiver_cnpj = client.cnpj
+            receiver_email = client.email
+            receiver_zipcode = client.zipcode
+            receiver_street = client.street
+            receiver_number = client.number
+            receiver_city = client.city
+            receiver_state = client.state
+            receiver_phone = client.phone
+            Invoice.objects.create(sale_order=sale_order,
+                                    emitter_name=emitter_name,
+                                    emitter_fantasy_name=emitter_fantasy_name,
+                                    emitter_state_registration=emitter_state_registration,
+                                    emitter_cnpj=emitter_cnpj,
+                                    emitter_email=emitter_email,
+                                    emitter_zipcode=emitter_zipcode,
+                                    emitter_street=emitter_street,
+                                    emitter_number=emitter_number,
+                                    emitter_city=emitter_city,
+                                    emitter_state=emitter_state,
+                                    emitter_phone=emitter_phone,
+                                    receiver_name=receiver_name,
+                                    receiver_fantasy_name=receiver_fantasy_name,
+                                    receiver_cnpj=receiver_cnpj,
+                                    receiver_email=receiver_email,
+                                    receiver_zipcode=receiver_zipcode,
+                                    receiver_street=receiver_street,
+                                    receiver_number=receiver_number,
+                                    receiver_city=receiver_city,
+                                    receiver_state=receiver_state,
+                                    receiver_phone=receiver_phone,
+                                    access_key=access_key,
+                                    barcode=barcode)
         return redirect("invoices:open_invoice", id=sale_order.id)
     return redirect("invoices:open_invoice", id=sale_order.id)
